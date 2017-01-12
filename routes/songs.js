d46cbcc9cdb62f37cf67c956a05783f1a5028530
lmotisi <lez.girl@live.fr>
2017-01-12 15:29:35 +0100

    fix route

--- routes/songs.js
+++ routes/songs.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const _ = require('lodash');
 const SongService = require('../services/songService');
 
 const mandatoryKeys = ['title', 'album', 'artist'];
